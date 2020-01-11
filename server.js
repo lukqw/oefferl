5e02695c2e34ba8bf33667583e85caf28d293cd8
lukqw <lukqw@users.noreply.github.com>
2020-01-11 14:24:44 +0100

    add env variable for access header

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+console.log(process.env.ACCESS_HEADER);
 var express = require('express'),
     request = require('request'),
     app = express();
 
 app.all('*', function (req, res, next) {
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "http://lukqw.com");
-    //res.header("Access-Control-Allow-Origin", "*");
+    let originHeader = process.env.ACCESS_HEADER;
+    res.header("Access-Control-Allow-Origin", originHeader);
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
 
